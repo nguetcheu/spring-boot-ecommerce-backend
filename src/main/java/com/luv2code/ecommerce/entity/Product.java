0823cf216d2120f9b3df74d8d09f6ed46d0dc8c5
nguetcheu <dnguetcheu@gmail.com>
2022-12-04 02:46:10 +0100

    ajout du package entity et création de la classe Product

--- src/main/java/com/luv2code/ecommerce/entity/Product.java
+++ src/main/java/com/luv2code/ecommerce/entity/Product.java
@@ -1,2 +1,35 @@
-package com.luv2code.ecommerce.entity;public class Product {
+package com.luv2code.ecommerce.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "product")
+@Data
+public class Product {
+    private Long id;
+
+    private  String sku;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal unit_price;
+
+    private String imageUrl;
+
+    private boolean active;
+
+    private int unitsInStock;
+
+    private Date dateCreated;
+
+    private Date lastUpdated;
+
 }
