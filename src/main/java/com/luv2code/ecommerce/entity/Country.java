c5e554e7e3b3a9b985a9ac4cea6450c9162fe5ae
nguetcheu <dnguetcheu@gmail.com>
2023-05-22 15:26:39 +0200

    Ajout du code des entites country et state ok

--- src/main/java/com/luv2code/ecommerce/entity/Country.java
+++ src/main/java/com/luv2code/ecommerce/entity/Country.java
@@ -1,4 +1,28 @@
 package com.luv2code.ecommerce.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "country")
+@Getter
+@Setter
 public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int id;
+
+    @Column(name = "code")
+    public String code;
+
+    @Column(name = "name")
+    public String name;
+
+    @OneToMany(mappedBy = "country")
+    private List<State> states;
+
 }
--- src/main/java/com/luv2code/ecommerce/entity/State.java
+++ src/main/java/com/luv2code/ecommerce/entity/State.java
@@ -1,4 +1,19 @@
 package com.luv2code.ecommerce.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "state")
 public class State {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(table = "country_id")
+    private Country country_id;
 }
