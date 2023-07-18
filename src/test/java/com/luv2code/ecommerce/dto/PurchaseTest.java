7b80f0653800387ba633a2a7a24a2a5f1a6208c4
nguetcheu <dnguetcheu@gmail.com>
2023-07-18 15:19:39 +0200

    pagination et trie implementation

--- src/test/java/com/luv2code/ecommerce/dto/PurchaseTest.java
+++ src/test/java/com/luv2code/ecommerce/dto/PurchaseTest.java
@@ -1,4 +1,17 @@
+package com.luv2code.ecommerce.dto;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PurchaseTest {
-  
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getCustomer() {
+    }
 }
